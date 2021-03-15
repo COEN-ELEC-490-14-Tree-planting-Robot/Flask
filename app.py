37081801276fb1393cec6d87c0b4cc9355d8c852
Haiyi Zhou <haiyi@genielab.co>
2021-03-14 23:36:48 -0400

    fix socketio bug and create front end ui

--- app.py
+++ app.py
@@ -11,52 +11,19 @@ socketio=SocketIO(app)
 
 @socketio.on('connect')
 def ws_connect():
-    try:
-        #Read
-        f=open("test.txt", "r")
-        data= f.read()
-        _=int(json.loads(data).get("counter"))+int(1)
-        tem={"counter":_ }
-        print(tem)
-        f.close()
-        emit('user', tem, broadcast=True)
-        
-        #write
-        fw= open("test.txt", "w", encoding='utf-8')
-        fw.write(json.dump(tem))
-        fw.close()
-        emit('user', tem, broadcast=True)
-        
-        
-    except Exception as e:
-        fw=open("test.txt", "w", encoding='utf-8')
-        fw.write(json.dump({"counter":0}))
-        fw.close()
-        emit('user', {"counter":0}, broadcast=True)
+    print('socket connected')
         
 @socketio.on('disconnect')
 def ws_disconnect():
+    print('socket disconnected')
     
-    f=open("test.txt", "r")
-    data= f.read()
-    tem={"counter": int(json.loads(data).get("counter"))-int(1)}
-    f.close()
 
-    
-    #write
-    fw= open("test.txt", "r")
-    fw.write(json.dump(tem))
-    fw.close()
-    emit('user', tem, broadcast=True)
-    
-    
 @app.route('/', methods=["POST","GET"])
 
+
 def home():
-    f=open("test.txt", "r")
-    data = f.read()
-    data = {"counter": int(json.loads(data).get("counter"))}
-    return render_template("index.html", data=data)
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
